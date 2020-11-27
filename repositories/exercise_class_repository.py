ef05a559b2ed45b78921065f19c2276203ee659f
Katie Strong <katiestrong123@gmail.com>
2020-11-27 16:29:50 +0000

    select and select all added to exercise_class_repo

--- repositories/exercise_class_repository.py
+++ repositories/exercise_class_repository.py
@@ -12,9 +12,47 @@ def save(exercise_class):
     return exercise_class
   
 #   READ -- SELECT ALL
+def select_all():  
+    exercise_classes = [] 
 
+    sql = "SELECT * FROM exercise_classes"
+    results = run_sql(sql)
+
+    for row in results:
+        exercise_class = ExerciseClass(row['name'], row['type'], row['duration'], row['id'] )
+        exercise_classes.append(exercise_class)
+    return exercise_classes 
 
 #   READ -- SELECT ONE 
+def select_all():  
+    exercise_classes = None 
+
+    sql = "SELECT * FROM exercise_classes WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        exercise_class = ExerciseClass(row['name'], row['type'], row['duration'], result['id'] )
+        exercise_classes.append(exercise_class)
+    return exercise_class
+
+
+
+
+
+
+def select(id):
+    trainer = None
+    sql = "SELECT * FROM Trainers WHERE id = %s"  
+    values = [id] 
+    result = run_sql(sql, values)[0]
+    
+    if result is not None:
+        trainer = Trainer(result['name'], result['email'], result['phone'], result['specialism'], result['id'] )
+    return trainer
+
+
+
 
 
 #   DELETE -- DELETE ALL
